17b8e5b88d3f8677065eb04ab7c06785c6d50052
Masatomo Segawa <masa-segawa@masa-segawa.com>
2015-05-08 00:57:59 +0900

    ゲームロジック用のプレハブの作成

---
+++ BaseGameSystem/Assets/BaseGameSystem/GameLogic/End.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class End : GameLogic {
+
+	public override void Init_GameLogic ()
+	{
+		base.Init_GameLogic ();
+		Debug.Log ("おわりのはじまり!");
+
+	}
+
+	public override void Update_GameLogic ()
+	{
+		base.Update_GameLogic ();
+
+	}
+
+	public override void End_GameLogic ()
+	{
+		base.End_GameLogic ();
+		Debug.Log ("End_End!!");
+
+	}
+
+}
