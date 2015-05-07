3339a62c4e58c2738be7528f6a1a49f6357886c6
Masatomo Segawa <masa-segawa@masa-segawa.com>
2015-05-07 22:26:01 +0900

    Add Base Script

---
+++ BaseGameSystem/Assets/Script/GameModel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModel : Singleton<GameModel> {
+
+	public static bool isFreeze;
+
+	public void SetFreeze(){
+		isFreeze = true;
+	}
+
+	public void SetRetry(){
+		isFreeze = false;
+	}
+
+}
